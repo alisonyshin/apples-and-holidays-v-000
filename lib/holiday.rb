0ff1cdf4f6a48694f932a55943872093157d3be2
Alison Shin <alisonyshin@gmail.com>
2018-08-15 19:09:25 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -43,7 +43,8 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   # Christmas AND the New Year's arrays
   holiday_hash[:winter].each do |holiday, holiday_details|
     holiday_details << supply
-#end
+  end
+end
 
 
 def add_supply_to_memorial_day(holiday_hash, supply)
